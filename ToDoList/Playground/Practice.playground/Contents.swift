b39619008ac954ee6e410ba11cb570624006cf38
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-04-14 22:31:17 +0330

    error playground

--- ToDoList/Playground/Practice.playground/Contents.swift
+++ ToDoList/Playground/Practice.playground/Contents.swift
@@ -1,9 +1,16 @@
 import Foundation
 
-func omid() {
-    for i in 1...5 {
-        print(i)
+func fetch() -> Bool {
+    
+    guard let url = URL(string: "https://www.google.com") else { return false }
+    
+    let task = URLSession.shared.dataTask(with: url) { data, _, error in
+        if let error = error { return false }
+        guard let data = data else { return false }
+        print(data)
+        return true
     }
+    task.resume()
 }
 
-omid()
+fetch()
