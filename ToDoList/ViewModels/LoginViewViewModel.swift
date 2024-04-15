8e53697b7aa890b41e375cc3e9f17814f761708c
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-04-15 19:38:34 +0330

    work with firebase auth

--- ToDoList/ViewModels/LoginViewViewModel.swift
+++ ToDoList/ViewModels/LoginViewViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by Omid Heydarzadeh on 4/11/24.
 //
 
+import FirebaseAuth
 import Foundation
 
 class LoginViewViewModel: ObservableObject {
@@ -17,22 +18,27 @@ class LoginViewViewModel: ObservableObject {
     }
     
     func login() {
-        guard !email.trimmingCharacters(in: .whitespaces).isEmpty, !password.trimmingCharacters(in: .whitespaces).isEmpty else { 
-            
-            errorMessage = "Please fill in all fields."
+        guard validate() else {
             return
         }
         
-        // email@foo.com
-        guard email.contains("@") && email.contains(".") else {
-            errorMessage = "Please enter valid email."
-            return
-        }
-        
-        print("Called")
+        // Try log in
+        Auth.auth().signIn(withEmail: email, password: password)
     }
     
-    func validate() {
+    private func validate() -> Bool {
+        errorMessage = ""
         
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty, !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            errorMessage = "Please fill in all fields."
+            return false
+        }
+        
+        guard email.contains("@") && email.contains(".") else {
+            errorMessage = "Please enter valid email."
+            return false
+        }
+        
+       return true
     }
 }
