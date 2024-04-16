4d55b6ed463eb0d97372719488f562b24908d978
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-04-16 10:11:39 +0330

    main view add tab bar - tab view

--- ToDoList/Views/MainView.swift
+++ ToDoList/Views/MainView.swift
@@ -12,8 +12,17 @@ struct MainView: View {
     
     var body: some View {
         if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
-            // signed in
-            ToDoListView()
+            TabView {
+                ToDoListView()
+                    .tabItem {
+                        Label("Home", systemImage: "house")
+                    }
+                
+                ProfileView()
+                    .tabItem {
+                        Label("Profile", systemImage: "person.circle")
+                    }
+            }
         } else {
             LoginView()
         }
