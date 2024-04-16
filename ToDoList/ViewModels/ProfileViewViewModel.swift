2370f8497ef9cc47950a2ecbfebfdd9cdb248753
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-04-17 00:15:25 +0330

    profile view and log out functionality complete

--- ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -28,6 +28,10 @@ class ProfileViewViewModel: ObservableObject {
     }
     
     func logOut() {
-        
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print(error)
+        }
     }
 }
