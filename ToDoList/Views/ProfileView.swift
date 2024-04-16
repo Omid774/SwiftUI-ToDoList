a8b54498f0f090e213bcb424416eb31ff833ef17
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-04-17 00:08:59 +0330

    abstract profile with ViewBuilder and function

--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -14,45 +14,7 @@ struct ProfileView: View {
         NavigationView {
             VStack {
                 if let user = viewModel.user {
-                    Image(systemName: "person.circle")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(Color.blue)
-                        .frame(width: 125, height: 125)
-                        .padding()
-                    
-                    // Info: Name, Email, Member since
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Text("Name: ")
-                                .bold()
-                            
-                            Text(user.name)
-                        }
-                        .padding()
-                        HStack {
-                            Text("Email: ")
-                                .bold()
-                            Text(user.email)
-                        }
-                        .padding()
-                        HStack {
-                            Text("Member Since: ")
-                                .bold()
-                            Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
-                        }
-                        .padding()
-                    }
-                    .padding()
-                    
-                    // Sign out
-                    Button("Log Out") {
-                        viewModel.logOut()
-                    }
-                    .tint(.red)
-                    .padding()
-                  
-                    Spacer()
+                    profile(user: user)
                 } else {
                     Text("Loading Profile...")
                 }
@@ -62,6 +24,49 @@ struct ProfileView: View {
         .onAppear {
             viewModel.fetchUser()
         }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        // Info: Name, Email, Member since
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ")
+                    .bold()
+                
+                Text(user.name)
+            }
+            .padding()
+            HStack {
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }
+            .padding()
+            HStack {
+                Text("Member Since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+        }
+        .padding()
+        
+        // Sign out
+        Button("Log Out") {
+            viewModel.logOut()
+        }
+        .tint(.red)
+        .padding()
+      
+        Spacer()
     }
 }
 
