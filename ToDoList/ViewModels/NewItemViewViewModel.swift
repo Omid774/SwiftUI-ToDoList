ff3da07ee7307d401ee58f07cd722f04a108cfc9
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-04-16 11:17:47 +0330

    create date and save(dismiss this view) with alert

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -10,10 +10,23 @@ import Foundation
 class NewItemViewViewModel: ObservableObject {
     @Published var title = ""
     @Published var dueDate = Date()
+    @Published var showAlert = false
     
     init() {}
     
     func save() {
         
+    }
+    
+    var canSave: Bool {
+        guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
+        
+        guard dueDate >= Date().addingTimeInterval(-86400) else {
+            return false
+        }
+        
+        return true
     }
 }
--- ToDoList/Views/NewItemView.swift
+++ ToDoList/Views/NewItemView.swift
@@ -29,10 +29,17 @@ struct NewItemView: View {
                 
                 // Button
                 TLButton(title: "Save", background: .pink) {
-                    viewModel.save()
-                    newItemPresented = false
+                    if viewModel.canSave {
+                        viewModel.save()
+                        newItemPresented = false
+                    } else {
+                        viewModel.showAlert = true
+                    }
                 }
                 .padding()
+            }
+            .alert(isPresented: $viewModel.showAlert) {
+                Alert(title: Text("Error"), message: Text("Please fill in all fields and select due date that is today or newer."))
             }
         }
     }
