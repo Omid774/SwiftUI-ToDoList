efd6d67a4bbae54b19332b971f9c8f1b932c7434
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-04-17 00:02:36 +0330

    profile completed with view model change

--- ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -12,6 +12,21 @@ import Foundation
 class ProfileViewViewModel: ObservableObject {
     init() {}
     
+    @Published var user: User? = nil
+    
+    func fetchUser() {
+        guard let userId = Auth.auth().currentUser?.uid else { return }
+        
+        let db = Firestore.firestore()
+        db.collection("users").document(userId).getDocument { [weak self] snapshot, error in
+            guard let data = snapshot?.data(), error == nil else { return }
+            
+            DispatchQueue.main.async {
+                self?.user = User(id: data["id"] as? String ?? "", name: data["name"] as? String ?? "", email: data["email"] as? String ?? "", joined: data["joined"] as? TimeInterval ?? 0)
+            }
+        }
+    }
+    
     func logOut() {
         
     }
--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -13,36 +13,49 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
-                // Avatar
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundColor(Color.blue)
-                    .frame(width: 125, height: 125)
-                
-                // Info: Name, Email, Member since
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text("Name: ")
-                        Text("Omid Heydarzadeh")
+                if let user = viewModel.user {
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color.blue)
+                        .frame(width: 125, height: 125)
+                        .padding()
+                    
+                    // Info: Name, Email, Member since
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("Name: ")
+                                .bold()
+                            
+                            Text(user.name)
+                        }
+                        .padding()
+                        HStack {
+                            Text("Email: ")
+                                .bold()
+                            Text(user.email)
+                        }
+                        .padding()
+                        HStack {
+                            Text("Member Since: ")
+                                .bold()
+                            Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+                        }
+                        .padding()
                     }
-                    HStack {
-                        Text("Email: ")
-                        Text("Omid Heydarzadeh")
+                    .padding()
+                    
+                    // Sign out
+                    Button("Log Out") {
+                        viewModel.logOut()
                     }
-                    HStack {
-                        Text("Member Since: ")
-                        Text("Omid Heydarzadeh")
-                    }
+                    .tint(.red)
+                    .padding()
+                  
+                    Spacer()
+                } else {
+                    Text("Loading Profile...")
                 }
-                
-                // Sign out
-                Button("Log Out") {
-                    viewModel.logOut()
-                }
-                .tint(.red)
-              
-                Spacer()
             }
             .navigationTitle("Profile")
         }
